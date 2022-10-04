947956e4c3a282d945a1162bc8b7176cd693e1c4
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2022-10-04 07:13:55 -0400

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Silvanei  Martins on 04/10/22.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
